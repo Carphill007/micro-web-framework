9d68d99a7b80d7918b5c782d133bc4c83c937591
unknown <phillc@GBBL1ITLP0103.drl.local>
2013-08-16 14:10:16 +0100

    CSP Tried to add a real test to see if coverage report works on node.ci

--- test/test.js
+++ test/test.js
@@ -1,4 +1,6 @@
 var assert = require("assert");
+var EmployeeProvider = require('./employeeprovider.js').EmployeeProvider;
+
 describe('Array', function(){
   describe('#indexOf()', function(){
     it('should return -1 when the value is not present', function(){
@@ -6,4 +8,15 @@ describe('Array', function(){
       assert.equal(-1, [1,2,3].indexOf(0));
     })
   })
+})
+
+describe('EmployeeProvider', function(){
+	describe('getCollection', function(){
+		it('should be empty when there are no employees in the database', function(){
+			var employeeProvider= new EmployeeProvider('localhost', 27017);
+			employeeProvider.findAll(function(error, emps){
+				assert.equal(0, emps.length);
+			});
+		})
+	})
 })
