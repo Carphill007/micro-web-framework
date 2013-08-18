e634907c9c80b6c5c0df117936e0aac017bcc17e
Carl Phillips <c_phillips_2000@yahoo.co.uk>
2013-08-18 13:35:27 +0100

    Added batch script to launch dev environment

---
+++ StartDevEnv.bat
@@ -0,0 +1,5 @@
+cmd.exe C:\source\sandbox\micro-web-framework\
+
+C:\Users\Carl\AppData\Local\Google\Chrome\Application\chrome.exe https://trello.com/b/PkY6B4N3/micro-web-framework https://github.com/Carphill007/micro-web-framework https://node.ci/projects/Carphill007/micro-web-framework/master/
+
+notepad++
