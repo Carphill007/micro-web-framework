feb47adcb5cd257690fb127ee29dcd0c52a04b11
unknown <phillc@GBBL1ITLP0103.drl.local>
2013-08-19 13:50:08 +0100

    Added sinon to dev dependancies

--- test/test.js
+++ test/test.js
@@ -2,15 +2,6 @@ var assert = require("assert");
 var ConnectionManager = require('../connectionmanager.js').ConnectionManager;
 var EmployeeProvider = require('../employeeprovider.js').EmployeeProvider;
 
-describe('Array', function(){
-  describe('#indexOf()', function(){
-    it('should return -1 when the value is not present', function(){
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    })
-  })
-})
-
 describe('EmployeeProvider', function(){
 	describe('getCollection', function(){
 		it('should be empty when there are no employees in the database', function(){
